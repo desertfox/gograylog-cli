b6a41e1ed6ac579a0b6c5b144a7835c4c084d372
Rummy <cerealbh@gmail.com>
2023-08-24 18:08:22 -0500

    Updates to Search
    Updated search cmd fields flag.

--- cmd/search.go
+++ cmd/search.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/desertfox/gograylog"
 	"github.com/desertfox/gograylog-cli/util"
@@ -15,9 +16,16 @@ var (
 	streamid  string
 	frequency int
 	limit     int
+	fields    string
 	searchCmd = &cobra.Command{
-		Use: "search",
+		Use:   "search",
+		Short: "search --flag value \"graylog search query\"",
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("must pass query string")
+				os.Exit(1)
+
+			}
 			h, t, err := util.ReadFromDisk(savePath)
 			if err != nil {
 				fmt.Println(err)
@@ -38,7 +46,7 @@ var (
 				StreamID:    streamid,
 				QueryString: args[0],
 				Frequency:   frequency,
-				Fields:      []string{"message"}, //TODO
+				Fields:      strings.Split(fields, ","),
 				Limit:       limit,
 			}
 
@@ -54,8 +62,9 @@ var (
 )
 
 func init() {
-	searchCmd.PersistentFlags().StringVar(&streamid, "streamid", "", "streamid")
-	searchCmd.PersistentFlags().IntVar(&frequency, "frequency", 900, "frequency")
-	searchCmd.PersistentFlags().IntVar(&limit, "limit", 10000, "limit")
+	searchCmd.PersistentFlags().StringVar(&streamid, "streamid", "", "graylog streamid")
+	searchCmd.PersistentFlags().IntVar(&frequency, "frequency", 900, "search frequency in seconds")
+	searchCmd.PersistentFlags().IntVar(&limit, "limit", 10000, "limit of messages to return")
+	searchCmd.PersistentFlags().StringVar(&fields, "fields", "timestamp,message", "csv header fields to be returned")
 	loginCmd.MarkPersistentFlagRequired("streamid")
 }
